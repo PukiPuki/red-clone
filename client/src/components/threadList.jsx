3d179ccd1522980289e07f1b05caadf4e73e66be
pukipuki <dfwong5566@gmail.com>
2018-03-15 07:48:46 +0800

    added UI display for fetched threads

--- client/src/components/threadList.jsx
+++ client/src/components/threadList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Dialog from 'material-ui/Dialog';
 import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -9,27 +10,82 @@ import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui
 import { Flex, Box } from 'reflexbox';
 import ThreadBox from './threadBox';
 
+import { fetchThreads, createThread } from '../api/threadApi';
+
 
 class ThreadList extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            username:''
-        }
+  constructor(props){
+    super(props);
+    this.state={
+      threads: [],
+      open: false,
     }
+  }
+  componentDidMount() {
+    fetchThreads()
+      .then(res => this.setState({
+        threads: res.body
+      }));
+  }
+
+  handleToggle = () => {
+    this.setState({open: !this.state.open});
+  };
+
+  topicChangeHandler = (event, newValue) => {
+    this.setState({topic:newValue});
+  }
+
+  createThreadHandler = () => {
+    this.setState({open: false});
+    createThread(this.state.topic)
+      .then(res => {
+        this.setState({threads: res.body});
+      });
+    this.setState({topic:""});
+  }
+
   render() {
+    let threads = this.state.threads;
+
+    let MappedThreads = () => {
+      return threads.map(thread => {
+        return (
+          <ThreadBox
+            topic={thread.topic}
+            date={thread.date}
+            vote={thread.vote}/>
+        );
+      })
+    };
+
     return (
+      <div>
         <Paper style={{padding: 10 }}>
           <Toolbar>
             <ToolbarGroup>
-            <ToolbarTitle text="Threads" />
-            <ToolbarSeparator/>
-            <RaisedButton label="Create" primary={true} />
+              <ToolbarTitle text="Threads" />
+              <ToolbarSeparator/>
+              <RaisedButton label="Create" primary={true} onClick={this.handleToggle}/>
             </ToolbarGroup>
           </Toolbar>
-          <ThreadBox />
-          <ThreadBox />
+          <MappedThreads/>
         </Paper>
+        <Dialog
+          title="Create a new Topic"
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleToggle}
+          >
+          <TextField
+            hintText="Enter topic"
+            floatingLabelText="Topic"
+            fullWidth={true}
+            onChange = {this.topicChangeHandler}
+            />
+          <RaisedButton label="Create" primary={true} onClick={this.createThreadHandler}/>
+        </Dialog>
+      </div>
     );
   }
 }
