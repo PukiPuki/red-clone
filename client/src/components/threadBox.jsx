be793647732ca7670cb5a35e1d26772856651124
pukipuki <dfwong5566@gmail.com>
2018-03-15 07:47:33 +0800

    added buttons, vote and date UI to threadBox

--- client/src/components/threadBox.jsx
+++ client/src/components/threadBox.jsx
@@ -8,25 +8,35 @@ import FlatButton from 'material-ui/FlatButton';
 import { Flex, Box } from 'reflexbox';
 
 class ThreadBox extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            username:''
-        }
-    }
+  constructor(props){
+    super(props);
+  }
+
+  upVoteHandler = () => {
+  }
+
+  downVoteHandler = () => {
+  }
+
   render() {
     return (
-        <Paper style={{padding: 2, marginTop:10 }}>
-            <Flex p={1} align='center'>
-              <Box px={2} w={2/12}>
-                <FlatButton label="↑" primary={true} />
-                <FlatButton label="↓" secondary={true} />
-              </Box>
-              <Box px={2} w={10/12}>
-                Right
-              </Box>
-            </Flex>
-        </Paper>
+      <Paper style={{padding: 2, marginTop:10 }}>
+        <Flex p={1} align='center'>
+          <Box px={2} w={0/12}>
+            {this.props.votes}
+          </Box>
+          <Box px={2} w={2/12}>
+            <FlatButton label="↑" primary={true} />
+            <br/>
+            <FlatButton label="↓" secondary={true} />
+          </Box>
+          <Box px={2} w={8/12}>
+            {this.props.topic}
+            <br/>
+            <a style={{fontSize:10}}> {new Date(this.props.date).toString()} </a>
+          </Box>
+        </Flex>
+      </Paper>
     );
   }
 }
