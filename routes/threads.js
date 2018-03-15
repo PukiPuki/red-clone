5c3407594b9283ab0a819f95a609e9dee1871de1
pukipuki <dfwong5566@gmail.com>
2018-03-16 01:23:43 +0800

    clean up change var to let and const

--- routes/threads.js
+++ routes/threads.js
@@ -2,17 +2,16 @@ var express = require('express');
 var router = express.Router();
 var customStructure = require('./../utils/dataStructure');
 var customStore = new customStructure();
-console.log(customStore.last);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(customStore.top20);
+  res.json(customStore.top20);
 });
 
 router.post('/create', function(req, res, next) {
-  var threads = customStore.insert(req.body);
+  const threads = customStore.insert(req.body);
   if (threads == false) {
-    res.status(error('Message should not be longer than 255 characters!'));
+    res.status(500).send('Message should not be longer than 255 characters!');
   } else {
     res.json(threads);
   }
@@ -21,9 +20,9 @@ router.post('/create', function(req, res, next) {
 router.put('/:id/vote', function(req, res, next) {
   const id = req.params.id;
   const vote = req.body.vote;
-  var threads = customStore.update({id, vote});
+  const threads = customStore.update({id, vote});
   if (threads == undefined) {
-    res.status(error('vote not found'));
+    res.status(500).error('vote not found');
   } else {
     res.json(threads);
   }
